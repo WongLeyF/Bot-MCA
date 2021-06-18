8f95f2cd83e325e1498f0315d3c5b0e43dde8778
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-06-18 16:53:15 -0600

    fix: mongo path to discord-xp

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
 const colors = require("colors"); //this Package is used, to change the colors of our Console! (optional and doesnt effect performance)
 const fs = require("fs"); //this package is for reading files and getting their inputs
-require('discord-xp').setURL(process.env.mongoPath)
 require('dotenv').config();
 
 //Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
@@ -22,11 +21,11 @@ client.aliases = new Discord.Collection(); //an collection for all your command-
 client.categories = fs.readdirSync("./commands/"); //categories
 client.cooldowns = new Discord.Collection(); //an collection for cooldown commands of each user
 client.cooldownXP = new Discord.Collection(); //an collection for cooldown XP of each user
-
+require("discord-xp").setURL(process.env.mongoPath);
 //Loading files, with the client variable like Command Handler, Event Handler, ...
 ["command", "events"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
 });
+
 //login into the bot
 client.login(process.env.token);
-
