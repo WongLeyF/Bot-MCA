fdaf6e9947f7ebdcae6b1b4379a51d0fa8eb7abb
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-06-18 16:22:51 -0600

    feat: new error msg to title&desc lenght in embed

--- commands/☠️ Moderación/embed.js
+++ commands/☠️ Moderación/embed.js
@@ -2,39 +2,48 @@ const { MessageEmbed } = require("discord.js");
 const ee = require("../../botconfig/embed.json");
 const gm = require("../../botconfig/globalMessages.json");
 module.exports = {
-    name: "Embed",
-    category: "☠️ Moderación",
-    aliases: ["say-embed"],
-    cooldown: 2,
-    usage: "embed <Título> $$ <Descripción>",
-    memberpermissions:["VIEW_AUDIT_LOG"],
-    description: "Reenvía un mensaje enforma de Embed",
-    run: async (client, message, args, user, text, prefix) => {
-    try{
-      if(!args[0])
+  name: "Embed",
+  category: "☠️ Moderación",
+  aliases: ["say-embed"],
+  cooldown: 2,
+  usage: "embed <Título> && <Descripción>",
+  memberpermissions: ["VIEW_AUDIT_LOG"],
+  description: "Reenvía un mensaje enforma de Embed",
+  run: async (client, message, args, user, text, prefix) => {
+    try {
+      if (!args[0])
         return message.channel.send(new MessageEmbed()
-            .setColor(ee.wrongcolor)
-            .setFooter(ee.footertext, ee.footericon)
-            .setTitle(`:warning: | No colocaste un título o una descripción`)
-            .setDescription(`Uso: \`${prefix}embed <Título> $$ <Descripción>\``)
-        );
-      let userargs = args.join(" ").split("$$");
+          .setColor(ee.wrongcolor)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`:warning: | No colocaste un título o una descripción`)
+          .setDescription(`Uso: \`${prefix}embed <Título> && <Descripción>\``)
+        ).then(msg => msg.delete({ timeout: 10000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+
+      let userargs = args.join(" ").split("&&");
       let title = userargs[0];
       let desc = userargs.slice(1).join(" ")
+      if (title.length > 256 | desc.length > 256 )
+        return message.channel.send(new MessageEmbed()
+          .setColor(ee.wrongcolor)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`:warning: | No puedes poner mas de 256 caracteres`)
+          .setDescription(`Uso: \`${prefix}embed <Título> && <Descripción>\``)
+        ).then(msg => msg.delete({ timeout: 10000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
       message.channel.send(new MessageEmbed()
         .setColor(ee.color)
         .setFooter(ee.footertext, ee.footericon)
         .setTitle(title ? title : "")
         .setDescription(desc ? desc : "")
       )
+      message.delete()
     } catch (e) {
-        console.log(String(e.stack).bgRed)
-        return message.channel.send(new MessageEmbed()
-            .setColor(ee.wrongcolor)
-            .setFooter(ee.footertext, ee.footericon)
-            .setTitle(gm.titleError)
-            .setDescription(`\`\`\`${e.stack}\`\`\``)
-        );
+      console.log(String(e.stack).bgRed)
+      return client.users.get("275407251127140352").send(new MessageEmbed()
+        .setColor(ee.wrongcolor)
+        .setFooter(ee.footertext, ee.footericon)
+        .setTitle(gm.titleError)
+        .setDescription(`\`\`\`${e.stack}\`\`\``)
+      );
     }
   }
 }
