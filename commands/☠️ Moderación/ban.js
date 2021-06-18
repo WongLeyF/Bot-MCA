53a857f7e5f0cd02c355f8dce55bfadcace162b5
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-06-18 16:20:28 -0600

    doc: fix messages to ban cmd

--- commands/☠️ Moderación/ban.js
+++ commands/☠️ Moderación/ban.js
@@ -14,30 +14,30 @@ module.exports = {
             if(!args[0]) return message.channel.send(new MessageEmbed()
                 .setColor(ee.wrongcolor)
                 .setTitle(`:warning: | Por favor, especifica al usuario`)
-                .setDescription(`Uso: \`${prefix}kick <Tag/ID> [Razón]\``)
+                .setDescription(`Uso: \`${prefix}ban <Tag/ID> [Razón]\``)
             ).then(msg=>msg.delete({timeout: 10000}).catch(e=>console.log(gm.errorDeleteMessage.gray)));
             if(!member) return message.channel.send(new MessageEmbed()
                 .setColor(ee.wrongcolor)
                 .setDescription('❌ No pude encontrar a este usuario')
             ).then(msg=>msg.delete({timeout: 10000}).catch(e=>console.log(gm.errorDeleteMessage.gray)));
-            if(!member.kickable) return message.channel.send(new MessageEmbed()
+            if(!member.bannable) return message.channel.send(new MessageEmbed()
                 .setColor(ee.wrongcolor)
-                .setDescription('❌ No puedo expulsar a este usuario. Ya que es mod/admin o tiene un rol mas alto que el mio')
+                .setDescription('❌ No puedo banear a este usuario. Ya que es mod/admin o tiene un rol mas alto que el mio')
             ).then(msg=>msg.delete({timeout: 10000}).catch(e=>console.log(gm.errorDeleteMessage.gray)));
             if(member.id === message.author.id) return message.channel.send(new MessageEmbed()
                 .setColor(ee.wrongcolor)
-                .setDescription('❌ Estem, no puedes expulsarte a ti mismo...')
+                .setDescription('❌ Estem, no puedes banearte a ti mismo...')
             ).then(msg=>msg.delete({timeout: 10000}).catch(e=>console.log(gm.errorDeleteMessage.gray)));
             let reason = !args.slice(1).join(" ") ? 'Sin especificar': args.slice(1).join(" ");
             await member.ban({reason: reason})
             
             message.channel.send( new MessageEmbed()
             .setColor(ee.color)
-            .setTitle('Miembro Expulsado')
+            .setTitle('Miembro Baneado')
             .setThumbnail(member.user.displayAvatarURL())
-            .addField('Usuario expulsado', member, true)
+            .addField('Usuario Baneado', member, true)
             .addField('ID', member.id, true)
-            .addField('Expulsado por', message.author)
+            .addField('Baneado por', message.author)
             .addField('Razon', reason)
             .setImage('https://media1.tenor.com/images/0be06cf168b1fa90572791419407f679/tenor.gif')
             .setFooter(ee.footertext, ee.footericon)
