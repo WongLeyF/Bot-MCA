b6b97f96e8b5f012c98f147de81c77aeaf41b8d4
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-06-24 00:23:22 -0600

    feat: new rows for ro and ch that dont give exp

--- models/settingsXp.js
+++ models/settingsXp.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose')
-
+//delete mongoose.connection.models['settingXP'];
 const settingsXP = mongoose.Schema({
 
     _id: {
         type: String,
         required: true
     },
-    min_xp: { type: Number },
-    max_xp: { type: Number },
-    cooldown: { type: Number }
+    min_xp: { type: Number, default: 1 },
+    max_xp: { type: Number, default: 30 },
+    cooldown: { type: Number, default: 60 },
+    noChannels: {type: Array},
+    noRoles: {type: Array}
 });
 
-module.exports = mongoose.model('settingXP', settingsXP)
+module.exports = mongoose.model('settingsXP', settingsXP)
