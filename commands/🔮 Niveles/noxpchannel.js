b2feeec3a735bcdd3fd629f6e5248e70ef6903b2
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-06-24 00:25:41 -0600

    feat: new cmds to don't give xp

---
+++ commands/🔮 Niveles/noxpchannel.js
@@ -0,0 +1,153 @@
+const { MessageEmbed, WebhookClient } = require("discord.js")
+const ee = require("../../botconfig/embed.json")
+const gm = require("../../botconfig/globalMessages.json")
+const settingsXP = require("../../models/settingsXp")
+const mongo = require('../../handlers/mongo')
+const { removeItemFromArr } = require("../../handlers/functions")
+
+module.exports = {
+    name: "noXPchannel",
+    description: "Muestra el cooldown por mensaje, asigna un cooldown o establecelo por defecto",
+    category: "🔮 Niveles",
+    cooldown: 5,
+    memberpermissions: ["MANAGE_CHANNELS", "MANAGE_GUILD"],
+    usage: "levels <enable/disable>",
+    run: async (client, message, args, user, text, prefix) => {
+        try {
+            await mongo().then(async mongoose => {
+                try {
+                    const guildID = message.guild.id
+                    let channel, data = await settingsXP.findOne({ _id: guildID })
+                    switch (args[0]) {
+                        case "add":
+                            if (args[1]) {
+                                if (args[1].includes(`@`)) return await message.reply(new MessageEmbed()
+                                    .setColor(ee.wrongcolor)
+                                    .setTitle("⚠ No XP Channels")
+                                    .setDescription(`Por favor escribe un canal valido`)
+                                ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                                if (isNaN(args[1])) channel = await message.guild.channels.cache.find(c => c.id === args[1].slice(2, -1))
+                                if (!isNaN(args[1])) channel = await message.guild.channels.cache.find(c => c.id === args[1])
+                                if (channel == undefined) channel = message.guild.channels.cache.find(c => c.name === args[1])
+                                if (args[1] == "all") {
+                                    const newArray = message.guild.channels.cache.filter(r => r.type == "text").map(r => r.id)
+                                    data.noChannels = newArray
+                                    await data.save()
+                                    return message.channel.send(new MessageEmbed()
+                                        .setColor(ee.checkcolor)
+                                        .setTitle("⚠ No XP Channels")
+                                        .setDescription(`Se agregaron correctamente todos los canales de la lista`)
+                                    )
+                                }
+                                if (!channel) return await message.reply(new MessageEmbed()
+                                    .setColor(ee.wrongcolor)
+                                    .setTitle("⚠ No XP Channels")
+                                    .setDescription(`Por favor escribe un canal valido para asignar`)
+                                ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                                if (data) {
+                                    if (!data.noChannels.includes(channel.id))
+                                        data.noChannels.push(channel.id)
+                                    await data.save()
+                                    return message.channel.send(new MessageEmbed()
+                                        .setColor(ee.checkcolor)
+                                        .setTitle("⚠ No XP Channels")
+                                        .setDescription(`Se agrego correctamente a la lista el canal: ${args[1]}`)
+                                    )
+                                } else {
+                                    const newData = new settingsXP({
+                                        _id: guildID,
+                                        noChannels: [channel.id]
+                                    })
+                                    await newData.save()
+                                    return message.channel.send(new MessageEmbed()
+                                        .setColor(ee.checkcolor)
+                                        .setTitle("⚠ No XP Channels")
+                                        .setDescription(`Se agrego correctamente a la lista el canal: ${args[1]}`)
+                                    )
+                                }
+                            } else return await message.reply(new MessageEmbed()
+                                .setColor(ee.color)
+                                .setTitle(`⚠ Por favor, dime que realizar`)
+                                .setDescription(`Uso: \`${prefix}noXPchannel <list/add/remove> <Canal>\``)
+                            ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                        case "remove":
+                            if (args[1]) {
+                                if (isNaN(args[1])) channel = await message.guild.channels.cache.find(c => c.id === args[1].slice(2, -1))
+                                if (!isNaN(args[1])) channel = await message.guild.channels.cache.find(c => c.id === args[1])
+                                if (channel == undefined) channel = message.guild.channels.cache.find(c => c.name === args[1])
+                                if (args[1] == "all") {
+                                    const newArray = []
+                                    data.noChannels = newArray
+                                    await data.save()
+                                    return message.channel.send(new MessageEmbed()
+                                        .setColor(ee.checkcolor)
+                                        .setTitle("⚠ No XP Channels")
+                                        .setDescription(`Se borro correctamente todos los canales de la lista`)
+                                    )
+                                }
+                                if (!channel) return await message.reply(new MessageEmbed()
+                                    .setColor(ee.wrongcolor)
+                                    .setTitle("⚠ No XP Channels")
+                                    .setDescription(`Por favor escribe un canal valido para eliminar`)
+                                ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                                if (data) {
+                                    if (data.noChannels.includes(channel.id)) {
+                                        const newArray = removeItemFromArr(data.noChannels, channel.id)
+                                        data.noChannels = newArray
+                                        await data.save()
+                                        return message.channel.send(new MessageEmbed()
+                                            .setColor(ee.checkcolor)
+                                            .setTitle("⚠ No XP Channels")
+                                            .setDescription(`Se borro correctamente de la lista el canal: ${args[1]}`)
+                                        )
+                                    } else {
+
+                                        return message.channel.send(new MessageEmbed()
+                                            .setColor(ee.wrongcolor)
+                                            .setTitle("⚠ No XP Channels")
+                                            .setDescription(`No encontre ningun canal que se llame asi: ${args[1]}`)
+                                        )
+                                    }
+                                }
+                            } else return await message.reply(new MessageEmbed()
+                                .setColor(ee.color)
+                                .setTitle(`⚠ Por favor, dime que realizar`)
+                                .setDescription(`Uso: \`${prefix}noXPchannel <list/add/remove> <Canal>\``)
+                            ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                        case "list":
+                            if (data) {
+                                channel = await message.guild.channels.cache.map(c => c.id)
+                                const nochannelsMap = data.noChannels.map(m => channel.includes(m) ? "<#" + m + ">\n\n" : "")
+                                return message.channel.send(new MessageEmbed()
+                                    .setTitle("**Canales que no reciben XP**:")
+                                    .setDescription(`\n${nochannelsMap.length > 0 ? nochannelsMap.join(" ") : "**Ninguno**"}`)
+                                )
+                            }
+                            break;
+                        default:
+                            return await message.reply(new MessageEmbed()
+                                .setColor(ee.color)
+                                .setTitle(`⚠ Por favor, dime que realizar`)
+                                .setDescription(`Uso: \`${prefix}noXPchannel <list/add/remove> <Canal>\``)
+                            ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                    }
+                } finally {
+                    mongoose.connection.close()
+                }
+            })
+        } catch (e) {
+            console.log(String(e.stack).bgRed);
+            const webhookClient = new WebhookClient(process.env.webhookID, process.env.webhookToken);
+            const embed = new MessageEmbed()
+                .setColor(ee.wrongcolor)
+                .setFooter(ee.footertext, ee.footericon)
+                .setTitle(gm.titleError)
+                .setDescription(`\`\`\`${e.stack}\`\`\``)
+            await webhookClient.send('Webhook Error', {
+                username: message.guild.name,
+                avatarURL: message.guild.iconURL({ dynamic: true }),
+                embeds: [embed],
+            });
+        }
+    },
+};
---
+++ commands/🔮 Niveles/noxprole.js
@@ -0,0 +1,144 @@
+const { MessageEmbed, WebhookClient } = require("discord.js")
+const ee = require("../../botconfig/embed.json")
+const gm = require("../../botconfig/globalMessages.json")
+const settingsXP = require("../../models/settingsXp")
+const mongo = require('../../handlers/mongo')
+const { removeItemFromArr } = require("../../handlers/functions")
+
+module.exports = {
+    name: "noXProle",
+    description: "Muestra el cooldown por mensaje, asigna un cooldown o establecelo por defecto",
+    category: "🔮 Niveles",
+    cooldown: 5,
+    memberpermissions: ["MANAGE_ROLES", "MANAGE_GUILD"],
+    usage: "levels <enable/disable>",
+    run: async (client, message, args, user, text, prefix) => {
+        try {
+            await mongo().then(async mongoose => {
+                try {
+                    const guildID = message.guild.id
+                    let role, data = await settingsXP.findOne({ _id: guildID })
+                    switch (args[0]) {
+                        case "add":
+                            if (args[1]) {
+                                if (!args[1].includes(`@&`)) return await message.reply(new MessageEmbed()
+                                    .setColor(ee.wrongcolor)
+                                    .setTitle("⚠ No XP Role")
+                                    .setDescription(`Por favor escribe un role valido`)
+                                ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                                if (isNaN(args[1])) role = await message.guild.roles.cache.find(c => c.id === args[1].slice(3, -1))
+                                if (!isNaN(args[1])) role = await message.guild.roles.cache.find(c => c.id === args[1])
+                                if (role == undefined) role = message.guild.roles.cache.find(c => c.name === args[1])
+                                if (!role) return await message.reply(new MessageEmbed()
+                                    .setColor(ee.wrongcolor)
+                                    .setTitle("⚠ No XP Role")
+                                    .setDescription(`Por favor escribe un role valido para asignar`)
+                                ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                                if (data) {
+                                    if (!data.noRoles.includes(role.id))
+                                        data.noRoles.push(role.id)
+                                    await data.save()
+                                    return message.channel.send(new MessageEmbed()
+                                        .setColor(ee.checkcolor)
+                                        .setTitle("⚠ No XP Role")
+                                        .setDescription(`Se agrego correctamente a la lista el role: ${args[1]}`)
+                                    )
+                                } else {
+                                    const newData = new settingsXP({
+                                        _id: guildID,
+                                        noRoles: [role.id]
+                                    })
+                                    await newData.save()
+                                    return message.channel.send(new MessageEmbed()
+                                        .setColor(ee.checkcolor)
+                                        .setTitle("⚠ No XP Role")
+                                        .setDescription(`Se agrego correctamente a la lista el role: ${args[1]}`)
+                                    )
+                                }
+                            } else return await message.reply(new MessageEmbed()
+                                .setColor(ee.color)
+                                .setTitle(`⚠ Por favor, dime que realizar`)
+                                .setDescription(`Uso: \`${prefix}noXProle <list/add/remove> <Role>\``)
+                            ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                        case "remove":
+                            if (args[1]) {
+                                if (isNaN(args[1])) role = await message.guild.roles.cache.find(c => c.id === args[1].slice(3, -1))
+                                if (!isNaN(args[1])) role = await message.guild.roles.cache.find(c => c.id === args[1])
+                                if (role == undefined) role = message.guild.roles.cache.find(c => c.name === args[1])
+                                if (args[1] == "all") {
+                                    const newArray = []
+                                    data.noRoles = newArray
+                                    await data.save()
+                                    return message.channel.send(new MessageEmbed()
+                                        .setColor(ee.checkcolor)
+                                        .setTitle("⚠ No XP Role")
+                                        .setDescription(`Se borro correctamente todos los roles de la lista`)
+                                    )
+                                }
+                                if (!role) return await message.reply(new MessageEmbed()
+                                    .setColor(ee.wrongcolor)
+                                    .setTitle("⚠ No XP Role")
+                                    .setDescription(`Por favor escribe un role valido para eliminar`)
+                                ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                                if (data) {
+                                    if (data.noRoles.includes(role.id)) {
+                                        const newArray = removeItemFromArr(data.noRoles, role.id)
+                                        data.noRoles = newArray
+                                        await data.save()
+                                        return message.channel.send(new MessageEmbed()
+                                            .setColor(ee.checkcolor)
+                                            .setTitle("⚠ No XP Role")
+                                            .setDescription(`Se borro correctamente de la lista el role: ${args[1]}`)
+                                        )
+                                    } else {
+
+                                        return message.channel.send(new MessageEmbed()
+                                            .setColor(ee.wrongcolor)
+                                            .setTitle("⚠ No XP Role")
+                                            .setDescription(`No encontre ningun role que se llame asi: ${args[1]}`)
+                                        )
+                                    }
+                                }
+                            } else return await message.reply(new MessageEmbed()
+                                .setColor(ee.color)
+                                .setTitle(`⚠ Por favor, dime que realizar`)
+                                .setDescription(`Uso: \`${prefix}noXProle <list/add/remove> <Role>\``)
+                            ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+
+                        case "list":
+                            if (data) {
+                                role = await message.guild.roles.cache.map(c => c.id)
+                                const norolesMap = data.noRoles.map(m => role.includes(m) ? "<@&" + m + ">\n\n" : "")
+                                return message.channel.send(new MessageEmbed()
+                                    .setTitle("**Roles que no reciben XP**:")
+                                    .setDescription(`\n${norolesMap.length > 0 ? norolesMap.join(" ") : "**Ninguno**"}`)
+                                )
+                            }
+                            break;
+                        default:
+                            return await message.reply(new MessageEmbed()
+                                .setColor(ee.color)
+                                .setTitle(`⚠ Por favor, dime que realizar`)
+                                .setDescription(`Uso: \`${prefix}noXProle <list/add/remove> <Role>\``)
+                            ).then(msg => msg.delete({ timeout: 30000 }).catch(e => console.log(gm.errorDeleteMessage.gray)));
+                    }
+                } finally {
+                    mongoose.connection.close()
+                }
+            })
+        } catch (e) {
+            console.log(String(e.stack).bgRed);
+            const webhookClient = new WebhookClient(process.env.webhookID, process.env.webhookToken);
+            const embed = new MessageEmbed()
+                .setColor(ee.wrongcolor)
+                .setFooter(ee.footertext, ee.footericon)
+                .setTitle(gm.titleError)
+                .setDescription(`\`\`\`${e.stack}\`\`\``)
+            await webhookClient.send('Webhook Error', {
+                username: message.guild.name,
+                avatarURL: message.guild.iconURL({ dynamic: true }),
+                embeds: [embed],
+            });
+        }
+    },
+};
