5c34b39705080c927061dc3968f73c52a2144c2c
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-07-08 01:59:51 -0600

    fix: make directory .temp in bulks deletes

--- events/guild/messageDeleteBulk.js
+++ events/guild/messageDeleteBulk.js
@@ -11,13 +11,17 @@ module.exports = async (client, messages) => {
     const rand = Math.floor(Math.random() * 99)
     const filepath = `${fecha.toISOString().slice(0, 10)}_${rand}_${guildName}`
     const fs = require('fs');
+    
     try {
+        fs.mkdir('.temp', { recursive: true }, (err) => {
+            if (err) throw err;
+          });
         const filteredMessages = messages.filter(f => {
             if (f.author) if (f.author.bot === false) return f;
         }).map(fm =>
             `${fm.author.username}#${fm.author.discriminator} (${fm.author})  ${fm.content}\n\n`
         )
-        if (filteredMessages.length === 0) throw 10;
+        if (filteredMessages.length === 0) throw new Error('No hay mensajes');
         // write to a new file named 2pac.txt
         fs.writeFileSync(`.temp/${filepath}.txt`, filteredMessages.join(''), (err) => {
             // throws an error, you could also catch it here
@@ -36,6 +40,6 @@ module.exports = async (client, messages) => {
         channel.send(new MessageEmbed()
             .setColor('RED')
             .setDescription(`❌ No pude guardar los mensajes de: <#${messages.first().channel.id}>\nEran: ${messages.size} mensajes`)
-        ).then(msg => msg.delete({ timeout: 10000 }).catch());
+        )
     }
 }
