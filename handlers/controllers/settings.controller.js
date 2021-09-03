f025ae540b09316b382bc41e2c8205d1dc6b3846
WongLeyF <jumawongleyf@ittepic.edu.mx>
2021-09-02 23:28:03 -0600

    hotFix: correctly return of value with mongo

--- handlers/controllers/settings.controller.js
+++ handlers/controllers/settings.controller.js
@@ -4,49 +4,72 @@ module.exports = {
     //return string with the id of confession channel 
     getChannelConfession: async (message) => {
         const guildID = message.guild.id
-        const result = settingSchema.findOne({ _id: guildID });
-        return result?.confessionChannel
+        let result
+        await settingSchema.findOne({ _id: guildID }).then( res => {
+            result = res?.confessionChannel
+        });
+        return result
     },
     //return string with the id of level channel 
     getChannelLevels: async (message) => {
         const guildID = message.guild.id
-        const result = settingSchema.findOne({ _id: guildID });
-        return result && result?.levelChannel != "" && (result?.levelChannel != undefined) ? result?.levelChannel : message.channel.id
+        await settingSchema.findOne({ _id: guildID }).then( res => {
+            result = res?.levelChannel
+        });
+        return result ? result : message.channel.id
     },
     //return string with the id of log messages channel 
     getChannelLogsMessages: async (message) => {
         const guildID = message.guild.id
-        const result = settingSchema.findOne({ _id: guildID });
-        return result && result?.logsMessages != "" && (result?.logsMessages != undefined) ? result?.logsMessages : message.channel.id
+        let result
+        await settingSchema.findOne({ _id: guildID }).then( res => {
+            result = res?.logsMessages
+        });
+        return result
     },
     //return string with the id of log messages channel 
     getChannelLogsModeration: async (message) => {
         const guildID = message.guild.id
-        const result = settingSchema.findOne({ _id: guildID });
-        return result && result?.logsModeration != "" && (result?.logsModeration != undefined) ? result?.logsModeration : message.channel.id
+        let result
+        await settingSchema.findOne({ _id: guildID }).then( res => {
+            result = res?.logsMessages
+        });
+        return result
     },
     //return boolean with the status of message counter
     getMessageCount: async (message) => {
         const guildID = message.guild.id
-        const result = settingSchema.findOne({ _id: guildID });
-        return result?.messageCounter
+        let result
+        await settingSchema.findOne({ _id: guildID }).then( res => {
+            result = res?.messageCounter ? true : false
+        });
+        return result
     },
     //return boolean with the status of message counter
     getUpdateMessages: async (message) => {
         const guildID = message.guild.id
-        const result = settingSchema.findOne({ _id: guildID });
-        return result?.updatesMessages
+        let result
+        await settingSchema.findOne({ _id: guildID }).then( res => {
+            result = res?.updatesMessages ? true : false
+        });
+        return result
     },
     //return boolean with the status of levels system
     getLevelSystem: async (message) => {
         const guildID = message.guild.id
-        const result = settingSchema.findOne({ _id: guildID });
-        return result?.levelSystem
+        let result
+        await settingSchema.findOne({ _id: guildID }).then( res => {
+            result = res?.levelSystem ? true : false
+        });
+        return result
     },
     //return string with the prefix of guild
     getPrefix: async (message) => {
         const guildID = message.guild.id
-        const result = await settingSchema.findOne({ _id: guildID });
-        return result?.prefix
+        let result
+        await await settingSchema.findOne({ _id: guildID }).then( res => {
+            result = res?.prefix
+        });
+        return result
     },
 }
